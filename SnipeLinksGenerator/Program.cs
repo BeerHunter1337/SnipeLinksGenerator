12c47b276651aa61e4c70918a1fad2f217c6ef35
CAHbl4 <a.matuzov@gmail.com>
2018-05-03 17:41:38 +0300

    Add project files.

---
+++ SnipeLinksGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnipeLinksGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
